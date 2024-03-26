fc859963ab189745e594734a0a5ad838c99f63f1
bitkunst <bitkunst23@gmail.com>
2024-03-26 15:42:52 +0900

    refactor: customMap

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,5 +1,11 @@
-import { User } from './User';
-import { Company } from './Company';
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+interface Mappable {
+    location: {
+        lat: number;
+        lng: number;
+    };
+}
 
 // Hiding Functionality
 // Limit access to the Google Map
@@ -16,22 +22,12 @@ export class CustomMap {
         });
     }
 
-    addUserMarker(user: User): void {
+    addMarker(mappable: Mappable): void {
         new google.maps.Marker({
             map: this.googleMap,
             position: {
-                lat: user.location.lat,
-                lng: user.location.lng,
-            },
-        });
-    }
-
-    addCompanyMarker(company: Company): void {
-        new google.maps.Marker({
-            map: this.googleMap,
-            position: {
-                lat: company.location.lat,
-                lng: company.location.lng,
+                lat: mappable.location.lat,
+                lng: mappable.location.lng,
             },
         });
     }
